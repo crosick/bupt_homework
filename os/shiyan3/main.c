d2047cd722bc39c374ce30d38e4edc655cdf1e1b
crosick <crmrc2014@gmail.com>
2015-11-29 16:16:10 +0800

    lalala

--- os/shiyan3/main.c
+++ os/shiyan3/main.c
@@ -4,8 +4,8 @@
 #include <semaphore.h>
 #include "buffer.h"
 
-void *producer();
-void *consumer();
+void *producer(void* pnumber);
+void *consumer(void* pnumber);
 
 pthread_mutex_t mutex;
 sem_t *empty, *full;
@@ -16,26 +16,46 @@ const char *full_name = "Full!"; //Die!!
 
 int main(int argc, char const *argv[])
 {
-
-    pthread_mutex_init(&mutex, NULL);
-    
     // wtf,osx sem_init 不能用
     // sem_init(&empty, 0, 5);
     // sem_init(&full, 0, 0);
 
-    empty = sem_open(empty_name, O_CREAT, 0644, 5);
-    full = sem_open(full_name, O_CREAT, 0644, 0);
+    int i;
+    int sleep_time = atoi(argv[1]);
+    int pro_thread_count = atoi(argv[2]);
+    int con_thread_count = atoi(argv[3]);
 
+    if ((empty = sem_open(empty_name, O_CREAT, 0644, 5)) == SEM_FAILED) {
+        perror("semaphore initilization");
+        exit(1);
+    }
 
+    if ((full = sem_open(full_name, O_CREAT, 0644, 0)) == SEM_FAILED) {
+        perror("semaphore initilization");
+        exit(1);
+    }
 
     pthread_t pro, con;
+    int *pnumber = malloc(sizeof(int));
 
-    pthread_create(&pro, NULL, producer, NULL);
-    pthread_create(&con, NULL, consumer, NULL);
+    for (i = 0; i < pro_thread_count; ++i)
+    {
+        *pnumber = i;
+        pthread_create(&pro, NULL, producer, (void*)pnumber);
+    }
 
-    pthread_join(pro, NULL);
-    pthread_join(con, NULL);
+    for (i = 0; i < con_thread_count; ++i)
+    {
+        *pnumber = i;
+        pthread_create(&con, NULL, consumer, (void*)pnumber);
+    }
+    
+    
 
+    // pthread_join(pro, NULL);
+    // pthread_join(con, NULL);
+
+    sleep(sleep_time);
     pthread_mutex_destroy(&mutex);
     
     // sem_destroy(&empty);
@@ -44,13 +64,16 @@ int main(int argc, char const *argv[])
     sem_unlink(empty_name);
     sem_unlink(full_name);
 
+    printf("program exit\n");
+
 
     return 0;
 }
 
-void *producer()
+void *producer(void* pnumber)
 {
     buffer_item item;
+    int number = *((int*)pnumber);
 
     while(1) {
         sleep(rand() % 3);
@@ -59,7 +82,7 @@ void *producer()
         sem_wait(empty);
         pthread_mutex_lock(&mutex);
         
-        printf("producer produced %d\n", item);
+        printf("producer%d produced %d\n", number, item);
         if (insert_item(item)){
             fprintf(stderr, "report error condition");
         }
@@ -69,9 +92,10 @@ void *producer()
     }
 }
 
-void *consumer()
+void *consumer(void* pnumber)
 {
     buffer_item item;
+    int number = *((int*)pnumber);
 
 
     while(1) {
@@ -85,7 +109,7 @@ void *consumer()
         if (remove_item(&item)){
             fprintf(stderr, "report error condition");
         }else{
-            printf("consumer consumed %d\n", item);
+            printf("consumer%d consumed %d\n", number, item);
         }
 
         pthread_mutex_unlock(&mutex);
